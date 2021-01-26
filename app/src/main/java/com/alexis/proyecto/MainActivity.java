b483656c779160093f090f99b71a708f7720152b
alexis1012 <L151100100@iztapalapa3.tecnm.mx>
2021-01-25 19:28:29 -0600

    prueva 1

--- app/src/main/java/com/alexis/proyecto/MainActivity.java
+++ app/src/main/java/com/alexis/proyecto/MainActivity.java
@@ -2,14 +2,21 @@ package com.alexis.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button crear, iniciar;
+    private EditText usuario;
+    private EditText contraseña;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
         crear = (Button)findViewById(R.id.crear);
         iniciar = (Button)findViewById(R.id.iniciar);
+        usuario = (EditText)findViewById(R.id.usuario);
+        contraseña = (EditText)findViewById(R.id.contreseña);
+        SharedPreferences preferences1 = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        usuario.setText( preferences1.getString("nombre","")   );
+        if(preferences1.contains("nombre")) {
+            Intent intent = new Intent(this, MainActivity2.class);
+            startActivity(intent);
+            finish();
+        }
 
         crear.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,11 +46,19 @@ public class MainActivity extends AppCompatActivity {
         iniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+                SharedPreferences preferences1 = getSharedPreferences("datos", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences1.edit();
+                String datos = usuario.getText().toString();
+                String Cont = contraseña.getText().toString();
 
+                editor.putString("nombre",datos);
+                editor.putString("contrasena",Cont);
+                editor.commit();
+
+                //Toast.makeText(this, "Bienvenido Tienda", Toast.LENGTH_LONG).show();
                 Intent inicio = new Intent(MainActivity.this, MainActivity14.class);
                 startActivity(inicio);
             }
         });
-
     }
 }
